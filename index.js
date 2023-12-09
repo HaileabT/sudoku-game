2914896e511d17f89ca7bf6a943fb8e4ef152f0f
HaileabT <Haileabtesfaye11@gmail.com>
2023-12-09 15:56:16 +0300

    Finished the skeleton of the sudoku board

---
+++ index.js
@@ -0,0 +1,17 @@
+let box = document.querySelectorAll(".box");
+let gameArray = new Array(9);
+for (let i = 0; i<9; i++){
+    gameArray[i] = new Array(9);
+}
+
+for (let i = 0; i<81; i++){
+    box[i].addEventListener("input", () =>{
+        if (isNaN(box[i].value)){
+            box[i].value = '';
+        }
+        if (box[i].value.length > 1){
+            box[i].value = box[i].value.slice(0, 1);
+        }
+        gameArray[Math.floor(i / 9)][i%9] = box[i].value;
+    })
+}
